94669ee89e10a8c907619513efddcdbc5bef7e9f
BingoIsaac <57237071+BingoIsaac@users.noreply.github.com>
2021-01-21 12:21:57 -0500

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,4 +1,6 @@
-local GitHubUser = 'BingoIsaac'
-local GitHubProject = 'Daydream'
-local GitHubFileLocation = '/79096434.lua'
-loadstring(game:HttpGet(('http://github.com/' .. GitHubUser .. '/' .. GitHubProject .. '/raw/main' .. GitHubFileLocation), true))()
+if game.PlaceId == 79096434 then
+  local GitHubUser = 'BingoIsaac'
+  local GitHubProject = 'Daydream'
+  local GitHubFileLocation = '/79096434.lua'
+  loadstring(game:HttpGet(('http://github.com/' .. GitHubUser .. '/' .. GitHubProject .. '/raw/main' .. GitHubFileLocation), true))()
+end
