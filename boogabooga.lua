0eff1e653eb3d0d9f4715239853262d6d8d9aac7
BingoIsaac <57237071+BingoIsaac@users.noreply.github.com>
2021-06-12 10:43:13 -0400

    Create boogabooga.lua

---
+++ boogabooga.lua
@@ -0,0 +1,91 @@
+-- Example made by twink marie
+local Material = loadstring(game:HttpGet('https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua'))()
+
+local X = Material.Load({
+    Title = 'Bingus Bingus',
+    Style = 3,
+    SizeX = 500,
+    SizeY = 350,
+    Theme = 'Dark'
+})
+
+local p = game.Players.LocalPlayer
+local c = p.Character
+local m = p:GetMouse()
+local h = c:FindFirstChild('HumanoidRootPart')
+
+local a = X.New({
+    Title = 'LocalPlayer'
+})
+
+speedKeybind = 'h'
+speedLoop = nil
+speedKeyDown = nil
+speedKeyUp = nil
+speedEnabled = false
+local a_a = a.Button({
+    Text = 'Speed',
+    Callback = function()
+        if speedEnabled == false then
+            speedEnabled = true
+            speedKeyDown = m.KeyDown:Connect(function(key)
+                if key == speedKeybind then
+                    speedLoop = true
+                    while speedLoop do
+                        h.CFrame = h.CFrame + h.CFrame.lookVector * 3
+                        wait()
+                    end
+                end
+            end)
+            speedKeyUp = m.KeyUp:Connect(function(key)
+                if key == speedKeybind then
+                    speedLoop = false
+                end
+            end)
+        end
+    end,
+    Menu = {
+        Information = function(self)
+            X.Banner({
+                Text = 'Apply speed to character; removes on death.'
+            })
+        end
+    }
+})
+
+local a_b = a.Button({
+    Text = 'Stop Speed',
+    Callback = function()
+        if speedEnabled == true then
+            speedEnabled = false
+            speedKeyDown:Disconnect()
+            speedKeyUp:Disconnect()
+        end
+    end,
+    Menu = {
+        Information = function(self)
+            X.Banner({
+                Text = 'Remove speed from character.'
+            })
+        end
+    }
+})
+
+local a_c = a.TextField({
+    Text = 'Speed Keybind',
+    Callback = function(Value)
+        speedKeybind = tostring(Value)
+    end,
+    Menu = {
+        Information = function(self)
+            X.Banner({
+                Text = 'Keybind for speed.'
+            })
+        end,
+        Default = function(self)
+            X.Banner({
+                Text = 'Default for speed is "h".'
+            })
+        end
+    }
+})
